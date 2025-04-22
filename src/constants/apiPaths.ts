a1a36f76ee93e3227ce03c34cd89290c74e9d380
Ruben Csepke <ruben.csepke@gmail.com>
2025-04-22 18:06:07 +0200

    feat: update links

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://l32255sj64.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://jf8rgvle91.execute-api.eu-central-1.amazonaws.com/prod/api/profile/cart",
   import: "https://8s7b9ij3of.execute-api.eu-central-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://d11ce9ka1aoz71.cloudfront.net",
   cart: "https://jf8rgvle91.execute-api.eu-central-1.amazonaws.com/prod/api",
 };
 
